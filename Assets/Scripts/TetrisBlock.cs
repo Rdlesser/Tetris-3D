0e00d10670c8a6f61773e3b84edd916a08cb5fba
Robert Lesser <rdlesser@gmail.com>
2019-12-18 18:23:09 +0200

    Started working on the tetris block functionality

---
+++ Assets/Scripts/TetrisBlock.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisBlock : TetrisElement
+{
+
+    private float _previousFallTime;
+    private float _fallTime = 1.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - _previousFallTime > _fallTime)
+        {
+            transform.position += Vector3.down;
+            _previousFallTime = Time.time;
+        }
+    }
+
+    private bool CheckValidMove()
+    {
+        return false;
+    }
+}
